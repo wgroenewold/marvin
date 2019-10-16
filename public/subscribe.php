10960d1603ad8571486413e39e022f95c6b7e496
wouter <wouter@gho.nl>
2019-10-16 10:28:19 +0200

    *sigh

--- public/subscribe.php
+++ public/subscribe.php
@@ -12,27 +12,27 @@ if($input){
         var_dump($input);
     }else{
         //filter bot messages
-        if(isset($data['event']['username']) == false && (isset($data['event']['message']['username']) && $data['event']['username'] != 'marvin')){
-            $args = array(
-                'text' => $data['event']['text'],
-                'channel' => $data['event']['channel'],
-                'created_at' => date('Y-m-d H:i:s'),
-            );
-
-            $instance->db->create('messages', $args);
-
-            $text = explode(',', getenv('SUBSCRIBE_CONFIRMATIONS'));
-
-            $key = array_rand($text);
-            $text = $text[$key];
-            $text = trim($text);
+//        if(isset($data['event']['username']) == false){
+//            $args = array(
+//                'text' => $data['event']['text'],
+//                'channel' => $data['event']['channel'],
+//                'created_at' => date('Y-m-d H:i:s'),
+//            );
+//
+//            $instance->db->create('messages', $args);
+//
+//            $text = explode(',', getenv('SUBSCRIBE_CONFIRMATIONS'));
+//
+//            $key = array_rand($text);
+//            $text = $text[$key];
+//            $text = trim($text);
 
             $instance->slack->send('https://slack.com/api/chat.postMessage', array(
                 'token' => getenv('SLACK_TOKEN'),
                 'channel' => $data['event']['channel'],
-                'text' => $text,
+                'text' => $data['event']['username'],
             ));
-        }
+//        }
     }
     return null;
 }
@@ -40,3 +40,6 @@ if($input){
 //filter is nog niet helemaal fris
 
 //event.message.username != 'Marvin' event.username != Marvin
+
+
+// && (isset($data['event']['message']['username']) && $data['event']['username'] != 'marvin')
