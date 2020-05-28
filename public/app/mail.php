7146e95319479173b9f21ac830510a47eadbbf10
wouter <wouter@gho.nl>
2020-05-28 14:30:41 +0200

    mailer

--- public/app/mail.php
+++ public/app/mail.php
@@ -9,19 +9,15 @@ class marvin_mail{
 		$this->mail = new marvin_mail();
 		$this->email = getenv('MAIL_ADDRESS');
 		$this->subject = getenv('MAIL_SUBJECT');
+		$this->message = getenv('MAIL_MESSAGE');
 	}
 
 	public function create($args){
-//		$args = array(
-//			'user_id' => $data['event']['user'],
-//			'text' => $data['event']['text'],
-//			'channel' => $data['event']['channel'],
-//			'created_at' => date('Y-m-d H:i:s'),
-//		);
+		$message = str_replace(array('{USER}', '{MESSAGE}'), array($args['user_id'], $args['text']), $this->message);
 
 		$data = array(
-			'replyto' => '',
-			'message' => ''
+//			'replyto' => '',
+			'message' => $message,
 			);
 
 		$this->send($data);
@@ -29,7 +25,7 @@ class marvin_mail{
 
 	public function send($args){
 		$headers = 'From: marvin@services.ghonline.nl' . "\r\n" .
-		           'Reply-To: ' . $args['replyto'] . "\r\n" .
+//		           'Reply-To: ' . $args['replyto'] . "\r\n" .
 		           'X-Mailer: PHP/' . phpversion();
 
 		mail($this->email, $this->subject, $args['message'], $headers);
